fb5cab00c39fb913d4b2d4677f3ded647991b31e
12gkab <121481379+12gkab@users.noreply.github.com>
2024-10-18 21:33:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ module.exports = {
   ANTIWORD_MSG: process.env.ANTIWORD_MSG || "_*antiword deleted successfully*_",
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
   SESSION_ID: process.env.SESSION_ID || 'Jarvis_67d7_2b41_c267_f02a_e67f_2c34_14b9_0c8e',
-  LANG: process.env.LANG || 'EN',
+  LANG: process.env.LANG || 'EN,ML',
   SETVV: process.env.SETVV || 'DM',
   ELEVENLABS: process.env.ELEVENLABS,
   HANDLERS: process.env.HANDLERS || process.env.HANDLER || process.env.PREFIX || '.',
